8c54dd8462c34c3943884f2cdaa8c69b85ce1bb8
rednaxela700 <ohleg@windowslive.com>
2019-12-18 22:48:44 +0100

    added props to class Tweet

--- src/index.js
+++ src/index.js
@@ -3,18 +3,35 @@ import ReactDOM from "react-dom";
 import "./index.sass";
 import * as serviceWorker from "./serviceWorker";
 
-const TweetTime = () => <time>18th December</time>;
-const TweetUser = () => (
+const TweetData = {
+  user: {
+    name: "Aleksander Walczuk",
+    handle: "contact.pl"
+  },
+  date: new Date(),
+  text: "Hello World"
+};
+
+const TweetTime = props => {
+  const date = `${props.date.getDate()}
+   ${props.date.toLocaleString("pl-pl", { month: "long" })}`;
+  return <time>{date}</time>;
+};
+const TweetUser = ({ name, handle }) => (
   <span>
-    <strong>Aleksander Walczuk</strong>@contact.pl - 
+    <strong>{name}</strong>@{handle}
   </span>
-)
+);
+
 class Tweet extends React.Component {
   render() {
+    const { user, text, date } = this.props.tweet;
+
     return (
       <div>
-        <TweetUser/>
-        <TweetTime/>
+        <TweetUser name={user.name} />
+        <TweetTime date={date} />
+        <p>{text}</p>
       </div>
     );
   }
@@ -22,9 +39,7 @@ class Tweet extends React.Component {
 
 ReactDOM.render(
   <div>
-    <Tweet />
-    <Tweet />
-    <Tweet />
+    <Tweet tweet={TweetData} />
   </div>,
   document.getElementById("root")
 );
