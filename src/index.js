3d515df460536a1744750ab2d23fa512f065d249
rednaxela700 <ohleg@windowslive.com>
2019-12-18 19:28:12 +0100

    deleted App component, put content into index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './index.sass';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <div>
+    <strong>Aleksadner Walczuk</strong>  @btmpl - <time>18th Decemeber</time>
+    <p>
+      Hello World!
+    </p>
+  </div>
+,document.getElementById('root'))
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
