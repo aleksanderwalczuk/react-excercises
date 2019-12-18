730486ec7a9806948e8eb9f2df5c9eb6224f2f5a
rednaxela700 <ohleg@windowslive.com>
2019-12-18 21:17:51 +0100

    rendering 3 Tweets

--- src/index.js
+++ src/index.js
@@ -5,18 +5,24 @@ import * as serviceWorker from "./serviceWorker";
 
 class Tweet extends React.Component {
   render() {
-    const date = (new Date()).toString();
+    const date = new Date().toString();
     return (
       <div>
-        <strong>Aleksander Walczuk</strong>@contact.pl -
-        <time>{date}</time>
+        <strong>Aleksander Walczuk</strong>@contact.pl -<time>{date}</time>
         <p>Hello world!</p>
       </div>
     );
   }
 }
 
-ReactDOM.render(<Tweet />, document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    <Tweet />
+    <Tweet />
+    <Tweet />
+  </div>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
